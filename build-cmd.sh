42b171a6f0b7e556d5163c3459787a99aadbfa1e
Drake Arconis <lightdrake@gmail.com>
2014-07-22 17:04:28 -0400

    VS2013 solutions and build script fixes

--- build-cmd.sh
+++ build-cmd.sh
@@ -33,33 +33,33 @@ pushd "$FREETYPELIB_SOURCE_DIR"
         "windows")
             load_vsvars
             
-            build_sln "builds/win32/vc2013/freetype.sln" "LIB Debug|Win32" 
-            build_sln "builds/win32/vc2013/freetype.sln" "LIB Release|Win32" 
+            build_sln "builds/windows/vc2013/freetype.sln" "Debug|Win32" 
+            build_sln "builds/windows/vc2013/freetype.sln" "Release|Win32" 
 
             mkdir -p "$stage/lib/debug"
             mkdir -p "$stage/lib/release"
-            cp -a "objs/win32/vc2013/freetype239_D.lib" "$stage/lib/debug/freetype.lib"
-            cp -a "objs/win32/vc2013/freetype239.lib" "$stage/lib/release/freetype.lib"
+            cp -a "objs/win32/vc2013/freetype253_D.lib" "$stage/lib/debug/freetype.lib"
+            cp -a "objs/win32/vc2013/freetype253.lib" "$stage/lib/release/freetype.lib"
                 
             mkdir -p "$stage/include/freetype2/"
-            cp -a include/ft2build.h "$stage/include/freetype2/"
-            cp -a include/freetype "$stage/include/freetype2/"
+            cp -a include/*.h "$stage/include/freetype2/"
+            cp -a include/config "$stage/include/freetype2/config"
         ;;
 
         "windows64")
             load_vsvars
             
-            build_sln "builds/win32/vc2013/freetype.sln" "LIB Debug|x64" 
-            build_sln "builds/win32/vc2013/freetype.sln" "LIB Release|x64" 
+            build_sln "builds/windows/vc2013/freetype.sln" "Debug|x64" 
+            build_sln "builds/windows/vc2013/freetype.sln" "Release|x64" 
 
             mkdir -p "$stage/lib/debug"
             mkdir -p "$stage/lib/release"
-            cp -a "objs/win32/vc2013/freetype239_D.lib" "$stage/lib/debug/freetype.lib"
-            cp -a "objs/win32/vc2013/freetype239.lib" "$stage/lib/release/freetype.lib"
+            cp -a "objs/win64/vc2013/freetype253_D.lib" "$stage/lib/debug/freetype.lib"
+            cp -a "objs/win64/vc2013/freetype253.lib" "$stage/lib/release/freetype.lib"
                 
             mkdir -p "$stage/include/freetype2/"
-            cp -a include/ft2build.h "$stage/include/freetype2/"
-            cp -a include/freetype "$stage/include/freetype2/"
+            cp -a include/*.h "$stage/include/freetype2/"
+            cp -a include/config "$stage/include/freetype2/config"
         ;;
 
         "darwin")
