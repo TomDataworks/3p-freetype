9eab02c6d6f74979190771dba61de990c864758f
Drake Arconis <drake@alchemyviewer.org>
2014-11-28 15:57:03 -0500

    Add linux64 build support

--- build-cmd.sh
+++ build-cmd.sh
@@ -199,6 +199,78 @@ pushd "$FREETYPELIB_SOURCE_DIR"
 
             make distclean
         ;;
+
+        "linux64")
+            # Linux build environment at Linden comes pre-polluted with stuff that can
+            # seriously damage 3rd-party builds.  Environmental garbage you can expect
+            # includes:
+            #
+            #    DISTCC_POTENTIAL_HOSTS     arch           root        CXXFLAGS
+            #    DISTCC_LOCATION            top            branch      CC
+            #    DISTCC_HOSTS               build_name     suffix      CXX
+            #    LSDISTCC_ARGS              repo           prefix      CFLAGS
+            #    cxx_version                AUTOBUILD      SIGN        CPPFLAGS
+            #
+            # So, clear out bits that shouldn't affect our configure-directed build
+            # but which do nonetheless.
+            #
+            # unset DISTCC_HOSTS CC CXX CFLAGS CPPFLAGS CXXFLAGS
+
+            # Prefer gcc-4.8 if available.
+            if [ -x /usr/bin/gcc-4.8 -a -x /usr/bin/g++-4.8 ]; then
+                export CC=/usr/bin/gcc-4.8
+                export CXX=/usr/bin/g++-4.8s
+            fi
+
+            # Default target to 64-bit
+            opts="${TARGET_OPTS:--m64}"
+
+            # Handle any deliberate platform targeting
+            if [ -z "$TARGET_CPPFLAGS" ]; then
+                # Remove sysroot contamination from build environment
+                unset CPPFLAGS
+            else
+                # Incorporate special pre-processing flags
+                export CPPFLAGS="$TARGET_CPPFLAGS" 
+            fi
+
+            # Debug first
+            CFLAGS="$opts -g -Og" \
+                CXXFLAGS="$opts -g -Og" \
+                CPPFLAGS="-I$stage/packages/include/zlib" \
+                LDFLAGS="$opts -L$stage/packages/lib/debug -Wl,--exclude-libs,libz" \
+                ./configure --with-pic --with-zlib \
+                --prefix="$stage" --libdir="$stage"/lib/debug/
+            make
+            make install
+
+            # conditionally run unit tests
+            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                # make test
+                echo "No tests"
+            fi
+
+            make distclean
+
+            # Release last
+            CFLAGS="$opts -g -O2" \
+                CXXFLAGS="$opts -g -O2" \
+                CPPFLAGS="-I$stage/packages/include/zlib" \
+                LDFLAGS="$opts -L$stage/packages/lib/release -Wl,--exclude-libs,libz" \
+                ./configure --with-pic --with-zlib \
+                --prefix="$stage" --libdir="$stage"/lib/release/
+            make
+            make install
+
+            # conditionally run unit tests
+            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                # make test
+                echo "No tests"
+            fi
+
+            make distclean
+        ;;
+
     esac
     mkdir -p "$stage/LICENSES"
     cp docs/LICENSE.TXT "$stage/LICENSES/freetype.txt"
