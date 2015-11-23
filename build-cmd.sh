57938487273006c7c497c701449cb1bab24cd454
Cinders <cinder@cinderblocks.biz>
2015-11-22 22:21:15 -0700

    update darwin sdk and build only x86_64

--- build-cmd.sh
+++ build-cmd.sh
@@ -81,9 +81,9 @@ pushd "$FREETYPELIB_SOURCE_DIR"
             # sdk=/Developer/SDKs/MacOSX10.6.sdk/
             # sdk=/Developer/SDKs/MacOSX10.7.sdk/
             # sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk/
-            sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/
+            sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/
             
-            opts="${TARGET_OPTS:--arch i386 -arch x86_64 -iwithsysroot $sdk -mmacosx-version-min=10.8}"
+            opts="${TARGET_OPTS:--arch x86_64 -iwithsysroot $sdk -mmacosx-version-min=10.8}"
 
             # Debug first
             CFLAGS="$opts -gdwarf-2 -O0" \
